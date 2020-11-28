3e2e7b7848064583289cce3e3aa131d5b25872d9
Kommunarus <41257346+Kommunarus@users.noreply.github.com>
2020-11-28 22:56:10 +0300

    Add files via upload

--- flask/main.py
+++ flask/main.py
@@ -4,7 +4,6 @@ from os import makedirs as make_dirs
 from os.path import join as join_path
 from os.path import abspath
 from os.path import exists as file_exist
-import subprocess
 
 import json
 import uuid
@@ -13,9 +12,11 @@ import csv
 HTML_TEMPLATE_DIR = './templates'
 STATIC_DIR = './static'
 TEMP_DIR = STATIC_DIR+'/temp'
+UPLOAD_DIR = STATIC_DIR+"/uploads"
 
 app = Flask(__name__, static_url_path='', template_folder=HTML_TEMPLATE_DIR, static_folder=STATIC_DIR)
 app.config['DEBUG'] = True
+app.config['UPLOAD_FOLDER'] = UPLOAD_DIR
 CORS(app)
 
 
@@ -28,17 +29,12 @@ def main_page():
 def transform():
     request_data = request.get_json()
     input_text = request_data.get('inputText', None)
+    alias = request_data.get('alias', None)
     if input_text:
         req_uuid = str(uuid.uuid1())
-        # req_uuid = "7ec3d6ec-30c1-11eb-a8b1-48ba4e478f7f"
-        temp_dir = store_to_temp_dir(input_text, req_uuid) + '/'
+        req_uuid = "7ec3d6ec-30c1-11eb-a8b1-48ba4e478f7f"
+        temp_dir = store_to_temp_dir(input_text, alias, req_uuid) + '/'
         # Вызов функции тут
-
-        command = '/home/alex/anaconda3/envs/coref/bin/python'
-        path2script = '/home/alex/PycharmProjects/coref/pipline.py'
-        cmd = [command, path2script] + ['--outdir'] + [temp_dir]
-        subprocess.check_output(cmd, universal_newlines=True)
-
         response_object = get_result(req_uuid)
         return json.dumps(response_object)
 
@@ -48,12 +44,53 @@ def transform():
     return json.dumps(response_object)
 
 
-def store_to_temp_dir(text, req_id):
+@app.route('/uploadfile', methods=['POST'])
+def uploadfile():
+
+    if 'file' not in request.files:
+        return json.dumps({'error': 'No file part'})
+
+    file = request.files['file']
+    if file.filename == '':
+        return json.dumps({'error': 'No selected file'})
+
+    file_ext = file.filename.split('.')[-1]
+    if file_ext not in ['txt', 'docx']:
+        return json.dumps({'error': 'Wrong file extension'})
+
+    file_content = store_and_read_file(file, file_ext)
+    if file_content is not None:
+        return json.dumps({'inputText': file_content})
+
+    return json.dumps({'error': "Can't read file"})
+
+
+def store_and_read_file(file, file_ext):
+    file_uuid = str(uuid.uuid1())
+    upload_dir = join_path(UPLOAD_DIR, file_uuid)
+    make_dirs(upload_dir, exist_ok=True)
+    file_path = join_path(upload_dir, file.filename)
+    try:
+        file.save(file_path)
+    except:
+        return None
+
+    if file_ext == "txt":
+        return read_text_file_content(file_path)
+
+    return None
+
+
+def store_to_temp_dir(text, alias, req_id):
     temp_dir = abspath(join_path(TEMP_DIR, req_id))
     make_dirs(temp_dir, exist_ok=True)
     output_file = join_path(temp_dir, 'input.txt')
     with open(output_file, encoding='utf-8', mode='w') as f:
         f.write(text)
+    if alias:
+        alias_file = join_path(temp_dir, 'alias.txt')
+        with open(alias_file, encoding='utf-8', mode='w') as f:
+            f.write(alias)
     return temp_dir
 
 
@@ -87,10 +124,16 @@ def get_result(req_id):
 
 
 def read_text_file_content(file_path):
-    file_content = ''
-    with open(file_path, encoding='utf-8', mode='r') as f:
-        file_content = f.read()
-    return file_content
+    file_content = None
+    encodings = ['utf-8', 'cp1251']
+    for enc in encodings:
+        try:
+            with open(file_path, encoding=enc) as f:
+                file_content = f.read()
+        except UnicodeDecodeError:
+            pass
+        else:
+            return file_content
 
 
 def read_replacement_file(file_path):
