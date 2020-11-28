c9e5c37def8206cf97813bff8c80ef4d2d510c24
Kommunarus <41257346+Kommunarus@users.noreply.github.com>
2020-11-28 23:00:28 +0300

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -91,17 +91,19 @@ def main():
                 greet_bot.startWorkNN(file_name, file_id, last_chat_id)
             last_chat_name = last_update['message']['chat']['first_name']
 
-            if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
-                greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
-                today += 1
+            greet_bot.send_message(last_chat_id, '{}. Пришлите текстовый файл txt.'.format(last_chat_name))
 
-            elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
-                greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
-                today += 1
-
-            elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
-                greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
-                today += 1
+            # if last_chat_text.lower() in greetings and today == now.day and 6 <= hour < 12:
+            #     greet_bot.send_message(last_chat_id, 'Доброе утро, {}'.format(last_chat_name))
+            #     today += 1
+            #
+            # elif last_chat_text.lower() in greetings and today == now.day and 12 <= hour < 17:
+            #     greet_bot.send_message(last_chat_id, 'Добрый день, {}'.format(last_chat_name))
+            #     today += 1
+            #
+            # elif last_chat_text.lower() in greetings and today == now.day and 17 <= hour < 23:
+            #     greet_bot.send_message(last_chat_id, 'Добрый вечер, {}'.format(last_chat_name))
+            #     today += 1
 
         new_offset = last_update_id + 1
 
